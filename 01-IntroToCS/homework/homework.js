572370bf5454d3cd391c0eae3da6af16ec5065f9
Gerardo Monaco <monacogj@gmail.com>
2022-06-08 19:02:32 -0300

    Homework ToCS TEST OK

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -3,41 +3,36 @@
 function BinarioADecimal(num) {
   let numstring= num.toString();
   let suma=0;
-
-  if(typeof num === "number"){
-    for (i=0 , j=numstring.length-1; i<numstring.length ; i++ , j-- ){
+    for (let i=0 , j=numstring.length-1; i<numstring.length ; i++ , j-- ){
       suma+=Number(numstring[i])*(2**j);
     }
-    return suma;
-  }
-  
+  return  suma;
 }
 
 function DecimalABinario(num) {
-  if(typeof num !== "number"){
+
+  let suma= new String();
+  let resultado="";
+ 
+    while (num>0){
+      if(num>1){
+         suma+=(num%2).toString();
+         num= Math.floor(num/2);
+      }else if(num===1){
+        suma+="1";
+         num=0;
+      }
+     }
+     resultado= (suma.split("").reverse()).join("");
+     return resultado;
+      /*if(typeof num !== "number"){
 
     return console.log(parseInt(num).toString(2));
    }else{
      return num.toString(2);
  
-   }
- 
-   /*
+   }*/
    //Alternativa 
-   let suma= new String();
- 
-     while (num>0){
-       if(num>1){
-         suma+=(num%2).toString();
-         num= Math.floor(num/2);
-       }else if(num===1){
-        suma+="1";
-         num=0;
-       }
-     }
-  
-     console.log(Number((suma.split("").reverse()).join("")));
- */
 
 }
 
